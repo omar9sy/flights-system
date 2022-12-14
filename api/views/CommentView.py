629b3ec9c016cf76a9d7b165574cd02a6d10e337
omar <omaralbitar77@gmail.com>
2022-12-14 16:55:32 +0300

    update register

--- api/views/CommentView.py
+++ api/views/CommentView.py
@@ -2,17 +2,16 @@ from api.models import Comment, AppUser
 from api.serializers import CommentSerializer
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
-
-@permission_classes([IsAuthenticated])
 @extend_schema(
     responses=CommentSerializer,
     request=CommentSerializer
 )
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def get_comments(request):
     if request.method == 'GET':
         data = Comment.objects.all()
@@ -21,6 +20,7 @@ def get_comments(request):
     else:
         serializer = CommentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(author=AppUser.objects.get(pk=request.user.id))
+        author = AppUser.objects.get(pk=request.user.id)
+        serializer.save(author=author)
 
         return Response({'result': serializer.data}, status=status.HTTP_201_CREATED)
