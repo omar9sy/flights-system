85788f0aca601377e3b678741d8b163700e65796
omar <omaralbitar77@gmail.com>
2022-12-14 16:52:10 +0300

    update register

--- api/views/CommentView.py
+++ api/views/CommentView.py
@@ -2,12 +2,12 @@ from api.models import Comment, AppUser
 from api.serializers import CommentSerializer
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
 
-@permission_classes([IsAuthenticatedOrReadOnly])
+@permission_classes([IsAuthenticated])
 @extend_schema(
     responses=CommentSerializer,
     request=CommentSerializer
