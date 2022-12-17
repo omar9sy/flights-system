aa086aee7bd19037cd9cdd8fbff55a761c951644
omar <omaralbitar77@gmail.com>
2022-12-17 21:20:29 +0300

    update scheduler

--- api/apps.py
+++ api/apps.py
@@ -1,38 +1,6 @@
-import time
-
 from django.apps import AppConfig
-from django.core.mail import send_mail
-from django.utils.datetime_safe import datetime
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def send(self, user, trip):
-        send_mail(
-            'reminder of flight',
-            f'you have a flight at {trip.departure_date}',
-            'flightsmanager5@gmail.com',
-            [user.email],
-            fail_silently=False,
-        )
-
-    def run(self):
-        from .models import TripReservation
-
-        print('test')
-        while True:
-            data = TripReservation.objects.all()
-            for reservation in data:
-                data = datetime.now().date()
-                diff = reservation.trip.departure_date - data
-                if diff.days <= 1:
-                    print(reservation.user.email)
-                    self.send(reservation.user, reservation.trip)
-            break
-            # time.sleep(3600)
-
-    def ready(self):
-        self.run()
-
--- api/emails_task.py
+++ api/emails_task.py
@@ -3,15 +3,10 @@ from django.core.mail import send_mail
 from django.utils.datetime_safe import datetime
 
 
-def send(user, trip):
+def send(user):
     send_mail(
-        'reminder of flight',
-        f'''
-        this is a reminder that you have a flight
-        flight details
-        date: {trip.departure_date}
-        time: {trip.departure_time}                
-        ''',
+        'Subject here',
+        'Here is the message.',
         'flightsmanager5@gmail.com',
         [user.email],
         fail_silently=False,
@@ -19,10 +14,11 @@ def send(user, trip):
 
 
 def run():
+    print('test')
     data = TripReservation.objects.all()
     for reservation in data:
         data = datetime.now().date()
         diff = reservation.trip.departure_date - data
-        if diff.days <= 1:
+        if diff.days <= 2:
             print(reservation.user.email)
-            send(reservation.user, reservation.trip)
+            send(reservation.user)
